06e228bb574e16d134e62c8cd94f7137fe09162a
Eriks Ocakovskis <e.ocakovskis@gmail.com>
2017-02-07 10:56:46 +0200

    Add index link and fix bug

--- cart.php
+++ cart.php
@@ -1,4 +1,7 @@
+<a href="index.php">Back to product listing</a>
+<br>
 <?php
+  session_start();
   $product_id = intval($_POST["id"]);
   if (array_key_exists($product_id, $_SESSION["cart"])) {
       $_SESSION["cart"][$product_id] += 1;
